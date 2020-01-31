c02a3bdc64d59b782fa5804009eb658513f06272
k0skar <koskarnoza@gmail.com>
2020-01-31 06:05:53 +0200

    deleted unused

--- src/js/connector.js
+++
@@ -1,8 +0,0 @@
-
-export async function fetchAllProducts() {
-    const response = await fetch('http://localhost:3000/products');
-    const responseObj = await response.json();
-  
-    return responseObj
-  }
-
